849a8dc34104afe5bb15bcefbd464c8628ae9c4e
SanketZingade <sanketzingade19@gmail.com>
2024-01-25 13:41:07 +0530

    Add project files.

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.tbEmp_name = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tbEmp_id = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.rollno = new System.Windows.Forms.Label();
+            this.dgvEmployeeList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(801, 413);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(138, 48);
+            this.btnDisplay.TabIndex = 25;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(528, 254);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(332, 39);
+            this.tbEmail.TabIndex = 24;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(314, 257);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(109, 33);
+            this.email.TabIndex = 23;
+            this.email.Text = "Email Id";
+            // 
+            // tbEmp_name
+            // 
+            this.tbEmp_name.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmp_name.Location = new System.Drawing.Point(528, 164);
+            this.tbEmp_name.Name = "tbEmp_name";
+            this.tbEmp_name.Size = new System.Drawing.Size(332, 39);
+            this.tbEmp_name.TabIndex = 22;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(314, 167);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(79, 33);
+            this.name.TabIndex = 21;
+            this.name.Text = "Name";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(626, 413);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(123, 48);
+            this.btnReset.TabIndex = 20;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(464, 413);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 48);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(291, 413);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(120, 48);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tbEmp_id
+            // 
+            this.tbEmp_id.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmp_id.Location = new System.Drawing.Point(528, 76);
+            this.tbEmp_id.Name = "tbEmp_id";
+            this.tbEmp_id.Size = new System.Drawing.Size(332, 39);
+            this.tbEmp_id.TabIndex = 17;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(157, 413);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(96, 48);
+            this.btnInsert.TabIndex = 16;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // rollno
+            // 
+            this.rollno.AutoSize = true;
+            this.rollno.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollno.Location = new System.Drawing.Point(314, 79);
+            this.rollno.Name = "rollno";
+            this.rollno.Size = new System.Drawing.Size(162, 33);
+            this.rollno.TabIndex = 15;
+            this.rollno.Text = "Employee Id:";
+            // 
+            // dgvEmployeeList
+            // 
+            this.dgvEmployeeList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvEmployeeList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvEmployeeList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployeeList.Location = new System.Drawing.Point(181, 526);
+            this.dgvEmployeeList.Name = "dgvEmployeeList";
+            this.dgvEmployeeList.RowHeadersWidth = 51;
+            this.dgvEmployeeList.RowTemplate.Height = 24;
+            this.dgvEmployeeList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEmployeeList.Size = new System.Drawing.Size(758, 259);
+            this.dgvEmployeeList.TabIndex = 14;
+            this.dgvEmployeeList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployeeList_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 861);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.tbEmp_name);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.tbEmp_id);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.rollno);
+            this.Controls.Add(this.dgvEmployeeList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.TextBox tbEmp_name;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox tbEmp_id;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label rollno;
+        private System.Windows.Forms.DataGridView dgvEmployeeList;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,160 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int emp_id;
+        public MySqlConnection Connect()
+        {
+            string Con = "server=127.0.0.1; user=root; database=tables;password=";
+            MySqlConnection con = new MySqlConnection(Con);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return con;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValid())
+                {
+                    MySqlConnection con = Connect();
+                    MySqlCommand cmd = new MySqlCommand("insert into employee(id,emp_name,email_id) values(@id, @emp_name, @email_id)", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@id", tbEmp_id.Text);
+                    cmd.Parameters.AddWithValue("@emp_name", tbEmp_name.Text);
+                    cmd.Parameters.AddWithValue("@email_id", tbEmail.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Inserted Successfully", "Insert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    GetStudentsRecord();
+                    Reset();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Id must be Unique.");
+            }
+            
+        }
+
+        private void Reset()
+        {
+            emp_id = 0;
+            tbEmp_id.ResetText();
+            tbEmp_name.ResetText();
+            tbEmail.ResetText();
+            tbEmp_id.Focus();
+        }
+
+        private void GetStudentsRecord()
+        {
+
+            dgvEmployeeList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            MySqlConnection con = Connect();
+            String sql = "Select * from employee";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            DataTable dTable = new DataTable();
+            
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd;
+            MyAdapter.Fill(dTable);
+            dgvEmployeeList.DataSource = dTable;
+            con.Close();
+        }
+
+        private bool IsValid()
+        {
+            if (tbEmp_name.Text == string.Empty)
+            {
+                MessageBox.Show("Employee information is Required ", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (emp_id > 0)
+            {
+                MySqlConnection con = Connect();
+                MySqlCommand cmd = new MySqlCommand("Update employee set emp_name=@emp_name,email_id=@email_id where id = @id", con);
+                cmd.Parameters.AddWithValue("@id", tbEmp_id.Text);
+                cmd.Parameters.AddWithValue("@emp_name", tbEmp_name.Text);
+                cmd.Parameters.AddWithValue("@email_id", tbEmail.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Updated Successfully", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //GetStudentsRecord();
+                Reset();
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please Select an Employee to update his information", "Select?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (emp_id > 0)
+            {
+                MySqlConnection con = Connect();
+                MySqlCommand cmd = new MySqlCommand("Delete from employee where id = @id", con);
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@id", this.emp_id);
+
+                var abc = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Deleted Successfully", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //GetStudentsRecord();
+                Reset();
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please Select an Employee to Delete his information", "Select?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            GetStudentsRecord();
+        }
+
+        private void dgvEmployeeList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            emp_id = Convert.ToInt32(dgvEmployeeList.SelectedRows[0].Cells[0].Value);
+            tbEmp_id.Text = dgvEmployeeList.SelectedRows[0].Cells[0].Value.ToString();
+            tbEmp_name.Text = dgvEmployeeList.SelectedRows[0].Cells[1].Value.ToString();
+            tbEmail.Text = dgvEmployeeList.SelectedRows[0].Cells[2].Value.ToString();
+        }
+    }
+}
